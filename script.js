d3d131d3b279c6d0b72f7c7cdfad53269c7ca9a0
Matheus Bortolotto <matheusbortolol@hotmail.com>
2024-05-03 22:01:08 -0300

    Aula 09

---
+++ script.js
@@ -0,0 +1,24 @@
+function somar (num1=0,num2=0,num3=0){
+    return num1+num2+num3
+}
+
+function printNaTela(){
+    var resultado = somar(5,7)
+    console.log(resultado)
+}
+
+console.log("Primeira frase")
+
+function printNaTela(){
+    console.log("Segunda Frase")
+}
+printNaTela()
+console.log("Terceira frase")
+
+const pessoas = ['Caio', 'fulano', 'Ciclano', 15, true]
+
+console.log(pessoas,length)
+
+function escolhaComp(){
+    
+}
